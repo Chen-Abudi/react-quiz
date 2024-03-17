5c4330fd068c692b6d8e1f210c6d2998ba063c3f
Grace Chen Abudi <98914366+Chen-Abudi@users.noreply.github.com>
2024-03-17 17:35:42 +0200

    Feature/questions timer (#3)
    * feat: Add timer to questions
    
    * feat: Set and cleanup timeout and interval

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,7 +1,8 @@
-import { useState } from "react";
+import { useState, useCallback } from "react";
 
 import QUESTIONS from "../questions.js";
 import quizCompletedLogo from "../assets/quiz-complete.png";
+import QuestionTimer from "./QuestionTimer.jsx";
 
 function Quiz() {
   const [userAnswers, setUserAnswers] = useState([]);
@@ -9,11 +10,19 @@ function Quiz() {
   const activeQuestionIndex = userAnswers.length;
   const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
 
-  function handleSelectAnswer(selectedAnswer) {
+  const handleSelectAnswer = useCallback(function handleSelectAnswer(
+    selectedAnswer
+  ) {
     setUserAnswers((prevUserAnswers) => {
       return [...prevUserAnswers, selectedAnswer];
     });
-  }
+  },
+  []);
+
+  const handleSkipAnswer = useCallback(
+    () => handleSelectAnswer(null),
+    [handleSelectAnswer]
+  );
 
   if (quizIsComplete) {
     return (
@@ -33,6 +42,11 @@ function Quiz() {
   return (
     <div id="quiz">
       <div id="question">
+        <QuestionTimer
+          key={activeQuestionIndex}
+          timeout={10000}
+          onTimeout={handleSkipAnswer}
+        />
         <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
         <ul id="answers">
           {shuffledAnswers.map((answer) => (
